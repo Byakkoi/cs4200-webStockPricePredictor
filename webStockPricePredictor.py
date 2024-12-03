5b758feb7ed9f4801d09e67729c04e8239cd42bc
Byakkoi <104779419+Byakkoi@users.noreply.github.com>
2024-12-02 19:47:13 -0800

    Update webStockPricePredictor.py

--- webStockPricePredictor.py
+++ webStockPricePredictor.py
@@ -4,23 +4,24 @@ import numpy as np
 from keras.models import load_model
 import matplotlib.pyplot as plt
 import yfinance as yf
+from sklearn.preprocessing import MinMaxScaler
+from datetime import datetime
 
 st.title("Stock Price Predictor App")
 
-stock = st.text_input("Enter the Stock ID", "GOOG")
+stock = st.text_input("Enter the Stock ID", "AAPL")
 
-from datetime import datetime
 end = datetime.now()
 start = datetime(end.year-20,end.month,end.day)
 
-google_data = yf.download(stock, start, end)
+stock_data = yf.download(stock, start, end)
 
 model = load_model("Latest_stock_price_model.keras")
 st.subheader("Stock Data")
-st.write(google_data)
+st.write(stock_data)
 
-splitting_len = int(len(google_data)*0.7)
-x_test = pd.DataFrame(google_data.Close[splitting_len:])
+splitting_len = int(len(stock_data)*0.7)
+x_test = pd.DataFrame(stock_data.Close[splitting_len:])
 
 def plot_graph(figsize, values, full_data, extra_data = 0, extra_dataset = None):
     fig = plt.figure(figsize=figsize)
@@ -31,21 +32,22 @@ def plot_graph(figsize, values, full_data, extra_data = 0, extra_dataset = None)
     return fig
 
 st.subheader('Original Close Price and MA for 250 days')
-google_data['MA_for_250_days'] = google_data.Close.rolling(250).mean()
-st.pyplot(plot_graph((15,6), google_data['MA_for_250_days'],google_data,0))
+stock_data['MA_for_250_days'] = stock_data.Close.rolling(250).mean()
+st.pyplot(plot_graph((15,6), stock_data['MA_for_250_days'],stock_data,0))
 
 st.subheader('Original Close Price and MA for 200 days')
-google_data['MA_for_200_days'] = google_data.Close.rolling(200).mean()
-st.pyplot(plot_graph((15,6), google_data['MA_for_200_days'],google_data,0))
+stock_data['MA_for_200_days'] = stock_data.Close.rolling(200).mean()
+st.pyplot(plot_graph((15,6), stock_data['MA_for_200_days'],stock_data,0))
 
 st.subheader('Original Close Price and MA for 100 days')
-google_data['MA_for_100_days'] = google_data.Close.rolling(100).mean()
-st.pyplot(plot_graph((15,6), google_data['MA_for_100_days'],google_data,0))
+stock_data['MA_for_100_days'] = stock_data.Close.rolling(100).mean()
+st.pyplot(plot_graph((15,6), stock_data['MA_for_100_days'],stock_data,0))
 
 st.subheader('Original Close Price and MA for 100 days and MA for 250 days')
-st.pyplot(plot_graph((15,6), google_data['MA_for_100_days'],google_data,1,google_data['MA_for_250_days']))
+st.pyplot(plot_graph((15,6), stock_data['MA_for_100_days'],stock_data,1,stock_data['MA_for_250_days']))
 
-from sklearn.preprocessing import MinMaxScaler
+# debugging
+st.write(stock_data.columns)
 
 scaler = MinMaxScaler(feature_range=(0,1))
 scaled_data = scaler.fit_transform(x_test[['Close']])
@@ -61,21 +63,22 @@ x_data, y_data = np.array(x_data), np.array(y_data)
 
 predictions = model.predict(x_data)
 
-inv_pre = scaler.inverse_transform(predictions)
+inv_predictions = scaler.inverse_transform(predictions)
 inv_y_test = scaler.inverse_transform(y_data)
 
-ploting_data = pd.DataFrame(
+plotting_data = pd.DataFrame(
  {
-  'original_test_data': inv_y_test.reshape(-1),
-    'predictions': inv_pre.reshape(-1)
+    'original_test_data': inv_y_test.reshape(-1),
+    'predictions': inv_predictions.reshape(-1)
  } ,
-    index = google_data.index[splitting_len+100:]
+    index = stock_data.index[splitting_len+100:]
 )
 st.subheader("Original values vs Predicted values")
-st.write(ploting_data)
+st.write(plotting_data)
 
 st.subheader('Original Close Price vs Predicted Close price')
 fig = plt.figure(figsize=(15,6))
-plt.plot(pd.concat([google_data.Close[:splitting_len+100],ploting_data], axis=0))
+plt.plot(pd.concat([stock_data.Close[:splitting_len+100],plotting_data], axis=0))
 plt.legend(["Data- not used", "Original Test data", "Predicted Test data"])
 st.pyplot(fig)
+
